a1cdb37fea6ce042286c60afc2d70855b2974221
AngelicAndres <alangarcia9517@gmail.com>
2026-02-10 17:42:51 -0600

    Inicio del proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone'; // Importaciones específicas
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true, // <--- Esto indica que es Standalone
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton], // Aquí añades los componentes de Ionic que uses
 })
 export class HomePage {
   constructor() {}
+
+  manejarEvento() {
+    const titulo = document.getElementById('texto-interactivo');
+    if (titulo) {
+      titulo.innerText = "¡Evento Detectado!";
+      titulo.style.color = "#2dd36f";
+    }
+  }
 }
